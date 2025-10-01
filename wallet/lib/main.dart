4083aefe643a6284f47899e9ad5b3c51912709f7
Cristopher Garcia <garcacristopher3@gmail.com>
2025-09-30 21:21:39 -0500

    Added var pair

--- wallet/lib/main.dart
+++ wallet/lib/main.dart
@@ -33,11 +33,12 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
-
+    var pair = appState.current;
     return Scaffold(
       body: Column(
         children: [
           Text('A random AWESOME idea:'),
+          Text(pair.asLowerCase),
           Text(appState.current.asLowerCase),
           ElevatedButton(
             onPressed: () {
